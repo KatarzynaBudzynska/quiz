c72ae497f9212bead29ed925b35c974e518c1c44
Kasia <katarzynabudzynska04@gmail.com>
2019-01-26 20:54:09 +0100

    quiz

---
+++ .js
@@ -0,0 +1,22 @@
+var points= 0;
+
+var answersBtns = 
+document.querySelectorAll('.answer');
+
+var checkPointsBtn =
+document.querySelector('.check-points');
+
+var ShowCorrectAnswersBtn =
+document.querySelector('.show-answers');
+
+var cpointsContainer =
+document.querySelector('.points');
+
+
+
+
+
+
+
+
+
